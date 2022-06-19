785d0a52b4656ecf447bbd523a6cd5fbec71394f
asdasd-mjeesh <201300@nuos.edu.ua>
2022-06-19 11:57:22 +0300

    _

--- src/test/java/dao/ExpressionDaoTest.java
+++ src/test/java/dao/ExpressionDaoTest.java
@@ -9,32 +9,29 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ExpressionDaoTest {
+    ExpressionDao expressionDao = ExpressionDao.getInstance();
 
     @Test
     void save() {
         Expression expression = new Expression("test__", -13.37);
-        ExpressionDao expressionDao = ExpressionDao.getInstance();
         var result = expressionDao.save(expression);
         System.out.println(result);
     }
 
     @Test
     void findById() {
-        ExpressionDao expressionDao = ExpressionDao.getInstance();
         var result = expressionDao.findById(2L);
         System.out.println(result.orElse(null));
     }
 
     @Test
     void findAllTest() {
-        ExpressionDao expressionDao = ExpressionDao.getInstance();
         List<Expression> expressions = expressionDao.findAll();
         System.out.println(expressions);
     }
 
     @Test
     void updateTest() throws Exception {
-        ExpressionDao expressionDao = ExpressionDao.getInstance();
         Expression expression = expressionDao.findById(2L).orElseThrow(Exception::new);
         expression.setValue("updated value");
         boolean result = expressionDao.update(expression);
@@ -43,17 +40,14 @@ class ExpressionDaoTest {
 
     @Test
     void deleteTest() {
-        ExpressionDao expressionDao = ExpressionDao.getInstance();
         boolean result = expressionDao.deleteById(5L);
         assertTrue(result);
     }
 
     @Test
     void findAllByFilterTest() {
-        ExpressionDao expressionDao = ExpressionDao.getInstance();
         List<Expression> expressions = expressionDao.findAllByFilter(new ExpressionFilter(
                 null, 0.5, -13.37, 10, 0));
-
         System.out.println(expressions);
     }
 }
